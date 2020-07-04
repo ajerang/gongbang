f41ae7c6b1b7209d4f150a75f0d63b51e35ea8f9
Young Maeng <ykmaeng@gmail.com>
2020-07-04 09:58:07 +0900

    문구 수정

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const menuItems = new Map([
+  ['Hello Javascript', './Hello-Javascript.html'],
+  ['구구단1', './구구단1.html'],
+  ['구구단2', './구구단2.html'],
+  ['비밀번호1', './비밀번호1.html'],
+  ['비밀번호2', './비밀번호2.html'],
+  ['버튼자물쇠1', './자물쇠1.html'],
+  ['버튼자물쇠2', './자물쇠2.html'],
+]);
+const nodeMenu = document.querySelector('.menu');
+const contentObject = document.querySelector('.content > object');
+
+menuItems.forEach((_, key) => {
+  let div = document.createElement('div');
+  div.innerHTML = key;
+  div.onclick = handleMenuClick;
+  nodeMenu.appendChild(div);
+});
+
+function handleMenuClick(e) {
+  const menu = e.target.innerHTML;
+  contentObject.data = menuItems.get(menu);
+}
